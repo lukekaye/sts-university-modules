51358341ef7a5ec514b7cda714a1803381c440b1
lukekaye <12499643+lukekaye@users.noreply.github.com>
2023-05-11 19:58:56 +0100

    Initial commit with template structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysing Semantic Textual Similarity of university module catalogue entries for linking modules comparable in content.',
+    author='Luke Kaye',
+    license='MIT',
+)
