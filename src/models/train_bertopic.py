99e6f043fb7655594ef67318c64df6c4b79febd3
lukekaye <12499643+lukekaye@users.noreply.github.com>
2023-06-25 23:07:20 +0100

    Update train_bertopic.py
    Add functionality to train a BERTopic model backed by the fitted BigBird-TSDAE model; some changes to variable names to better distinguish fine-tuned models

--- src/models/train_bertopic.py
+++ src/models/train_bertopic.py
@@ -28,17 +28,20 @@ def main():
     train_embeddings_path = project_dir.joinpath('data/processed/train_document_embeddings.pkl')
     with open(train_embeddings_path, "rb") as embeddings_input:
       saved_embeddings = pickle.load(embeddings_input)
-      train_simcse_embeddings = saved_embeddings['train_simcse_embeddings']
-      train_ct_embeddings = saved_embeddings['train_ct_embeddings']
+      train_longformer_simcse_embeddings = saved_embeddings['train_longformer_simcse_embeddings']
+      train_longformer_ct_embeddings = saved_embeddings['train_longformer_ct_embeddings']
+      train_bigbird_tsdae_embeddings = saved_embeddings['train_bigbird_tsdae_embeddings']
 
     # get documents
     docs = train['Concatenated']
 
     # load our fine-tuned models
-    model_simcse_path = project_dir.joinpath('models/longformer-simcse')
-    model_simcse = SentenceTransformer(model_simcse_path)
-    model_ct_path = project_dir.joinpath('models/longformer-ct')
-    model_ct = SentenceTransformer(model_ct_path)
+    model_longformer_simcse_path = project_dir.joinpath('models/longformer-simcse')
+    model_longformer_simcse = SentenceTransformer(model_longformer_simcse_path)
+    model_longformer_ct_path = project_dir.joinpath('models/longformer-ct')
+    model_longformer_ct = SentenceTransformer(model_longformer_ct_path)
+    model_bigbird_tsdae_path = project_dir.joinpath('models/bigbird-tsdae')
+    model_bigbird_tsdae = SentenceTransformer(model_bigbird_tsdae_path)
 
     # parameters for dimensionality reduction (UMAP)
     # the defaults, except random_state is set to 1 for reproducibility
@@ -53,23 +56,30 @@ def main():
     representation_model = KeyBERTInspired(random_state = 1)
 
     # BERTopic c-TF-IDF modelling
-    # SimCSE, CT with in-batch negatives
-    topic_model_simcse = BERTopic(embedding_model = model_simcse,
-                                  umap_model = umap_model,
-                                  vectorizer_model = vectoriser,
-                                  representation_model = representation_model)
-    topic_model_ct = BERTopic(embedding_model = model_ct,
-                              umap_model = umap_model,
-                              vectorizer_model = vectoriser,
-                              representation_model = representation_model)
-    fitted_topic_model_simcse = topic_model_simcse.fit(docs, train_simcse_embeddings)
-    fitted_topic_model_ct = topic_model_ct.fit(docs, train_ct_embeddings)
+    # Longformer-SimCSE, Longformer-CT with in-batch negatives, BigBird-TSDAE
+    topic_model_longformer_simcse = BERTopic(embedding_model = model_longformer_simcse,
+                                             umap_model = umap_model,
+                                             vectorizer_model = vectoriser,
+                                             representation_model = representation_model)
+    topic_model_longformer_ct = BERTopic(embedding_model = model_longformer_ct,
+                                         umap_model = umap_model,
+                                         vectorizer_model = vectoriser,
+                                         representation_model = representation_model)
+    topic_model_bigbird_tsdae = BERTopic(embedding_model = model_bigbird_tsdae,
+                                         umap_model = umap_model,
+                                         vectorizer_model = vectoriser,
+                                         representation_model = representation_model)
+    fitted_topic_model_longformer_simcse = topic_model_longformer_simcse.fit(docs, train_longformer_simcse_embeddings)
+    fitted_topic_model_longformer_ct = topic_model_longformer_ct.fit(docs, train_longformer_ct_embeddings)
+    fitted_topic_model_bigbird_tsdae = topic_model_bigbird_tsdae.fit(docs, train_bigbird_tsdae_embeddings)
 
     # save fitted BERTopic models
-    save_path_simcse = str(project_dir.joinpath(f'models/longformer-simcse-bertopic'))
-    save_path_ct = str(project_dir.joinpath(f'models/longformer-ct-bertopic'))
-    fitted_topic_model_simcse.save(save_path_simcse, serialization = 'pytorch', save_ctfidf = True)
-    fitted_topic_model_ct.save(save_path_ct, serialization = 'pytorch', save_ctfidf = True)
+    save_path_longformer_simcse = str(project_dir.joinpath(f'models/longformer-simcse-bertopic'))
+    save_path_longformer_ct = str(project_dir.joinpath(f'models/longformer-ct-bertopic'))
+    save_path_bigbird_tsdae = str(project_dir.joinpath(f'models/bigbird-tsdae-bertopic'))
+    fitted_topic_model_longformer_simcse.save(save_path_longformer_simcse, serialization = 'pytorch', save_ctfidf = True)
+    fitted_topic_model_longformer_ct.save(save_path_longformer_ct, serialization = 'pytorch', save_ctfidf = True)
+    fitted_topic_model_bigbird_tsdae.save(save_path_bigbird_tsdae, serialization = 'pytorch', save_ctfidf = True)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
