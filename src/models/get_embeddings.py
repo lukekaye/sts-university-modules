cbf7056fdb9be4f2b82aaffb8376e9de289fb200
lukekaye <12499643+lukekaye@users.noreply.github.com>
2023-06-30 00:15:43 +0100

    Update get_embeddings.py
    Add functionality to generate embeddings for Longformer, BigBird, BigBird-SimCSE and BigBird-CT

--- src/models/get_embeddings.py
+++ src/models/get_embeddings.py
@@ -3,13 +3,13 @@ import logging
 from dotenv import find_dotenv, load_dotenv
 from pathlib import Path
 import pandas as pd
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, models
 import torch
 import pickle
 
 def main():
     '''
-    Use the trained model to generate document embeddings for each model
+    Use the pre-trained and fine-tuned models to generate document embeddings
     Embeddings are generated for both training and testing data
     All fine-tuned models are required for this script to work
     output saved to ../data/processed as train_document_embeddings.pkl
@@ -29,16 +29,42 @@ def main():
     train_path = project_dir.joinpath('data/processed/train.pkl')
     train = pd.read_pickle(train_path)
 
+    # load longformer
+    longformer_name = 'allenai/longformer-base-4096'
+    word_embedding_model = models.Transformer(longformer_name,
+                                              model_args = {'gradient_checkpointing': True})
+    pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
+    model_longformer = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+    # load bigbird
+    bigbird_name = 'google/bigbird-roberta-base'
+    word_embedding_model = models.Transformer(bigbird_name,
+                                              model_args = {'gradient_checkpointing': True})
+    pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
+    model_bigbird = SentenceTransformer(modules=[word_embedding_model, pooling_model])
     # load our fine-tuned Longformer-SimCSE model
     model_longformer_simcse_path = project_dir.joinpath('models/longformer-simcse')
     model_longformer_simcse = SentenceTransformer(model_longformer_simcse_path)
     # load our fine-tuned Longformer-CT with in-batch negatives model
     model_longformer_ct_path = project_dir.joinpath('models/longformer-ct')
     model_longformer_ct = SentenceTransformer(model_longformer_ct_path)
+    # load our fine-tuned BigBird-SimCSE model
+    model_bigbird_simcse_path = project_dir.joinpath('models/bigbird-simcse')
+    model_bigbird_simcse = SentenceTransformer(model_bigbird_simcse_path)
+    # load our fine-tuned BigBird-CT with in-batch negatives model
+    model_bigbird_ct_path = project_dir.joinpath('models/bigbird-ct')
+    model_bigbird_ct = SentenceTransformer(model_bigbird_ct_path)
     # load our fine-tuned BigBird-TSDAE model
     model_bigbird_tsdae_path = project_dir.joinpath('models/bigbird-tsdae')
     model_bigbird_tsdae = SentenceTransformer(model_bigbird_tsdae_path)
 
+    # generate Longformer document embeddings
+    train_longformer_embeddings = model_longformer.encode(sentences = train['Concatenated'].to_numpy(),
+                                                          batch_size = 16,
+                                                          show_progress_bar = True)
+    # generate Bigbird document embeddings
+    train_bigbird_embeddings = model_bigbird.encode(sentences = train['Concatenated'].to_numpy(),
+                                                    batch_size = 16,
+                                                    show_progress_bar = True)
     # generate Longformer-SimCSE document embeddings
     train_longformer_simcse_embeddings = model_longformer_simcse.encode(sentences = train['Concatenated'].to_numpy(),
                                                                         batch_size = 16,
@@ -47,7 +73,15 @@ def main():
     train_longformer_ct_embeddings = model_longformer_ct.encode(sentences = train['Concatenated'].to_numpy(),
                                                                 batch_size = 16,
                                                                 show_progress_bar = True)
-    # generate Longformer-CT document embeddings
+    # generate BigBird-SimCSE document embeddings
+    train_bigbird_simcse_embeddings = model_bigbird_simcse.encode(sentences = train['Concatenated'].to_numpy(),
+                                                                  batch_size = 16,
+                                                                  show_progress_bar = True)
+    # generate BigBird-CT document embeddings
+    train_bigbird_ct_embeddings = model_bigbird_ct.encode(sentences = train['Concatenated'].to_numpy(),
+                                                          batch_size = 16,
+                                                          show_progress_bar = True)
+    # generate bigbird-tsdae document embeddings
     train_bigbird_tsdae_embeddings = model_bigbird_tsdae.encode(sentences = train['Concatenated'].to_numpy(),
                                                                 batch_size = 16,
                                                                 show_progress_bar = True)
@@ -55,8 +89,12 @@ def main():
     # save document embeddings
     train_embeddings_output = project_dir.joinpath('data/processed/train_document_embeddings.pkl')
     with open(train_embeddings_output, "wb") as output:
-      pickle.dump({'train_longformer_simcse_embeddings': train_longformer_simcse_embeddings,
+      pickle.dump({'train_longformer_embeddings': train_longformer_embeddings,
+                   'train_bigbird_embeddings': train_bigbird_embeddings,
+                   'train_longformer_simcse_embeddings': train_longformer_simcse_embeddings,
                    'train_longformer_ct_embeddings': train_longformer_ct_embeddings,
+                   'train_bigbird_simcse_embeddings': train_bigbird_simcse_embeddings,
+                   'train_bigbird_ct_embeddings': train_bigbird_ct_embeddings,
                    'train_bigbird_tsdae_embeddings': train_bigbird_tsdae_embeddings}, 
                   output, 
                   protocol = pickle.HIGHEST_PROTOCOL)
