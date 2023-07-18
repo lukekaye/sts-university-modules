fb33b3168cfe4058c1f07d88c0e4585634b3b1e7
lukekaye <12499643+lukekaye@users.noreply.github.com>
2023-07-19 00:49:46 +0100

    Update train_doc2vec.py
    Functionality to fit multiple doc2vec models; each differs by the number of training epochs

--- src/models/train_doc2vec.py
+++ src/models/train_doc2vec.py
@@ -6,13 +6,33 @@ import pandas as pd
 import gensim
 
 
+def train_doc2vec(num_epochs, tagged_data):
+    '''
+    Trains a doc2vec model for the specified number of epochs and saves it to ../models
+    '''
+    # doc2vec model (dm = 1 is Distributed Memory approach as used in Seidel et al., 2020, set seed & workers for reproducibility)
+    doc2vec_model = gensim.models.doc2vec.Doc2Vec(vector_size = 256,
+                                                  dm = 1,
+                                                  min_count = 2,
+                                                  epochs = num_epochs,
+                                                  seed = 1,
+                                                  workers = 1)
+
+    # build vocabulary and train model
+    doc2vec_model.build_vocab(tagged_data)
+    doc2vec_model.train(tagged_data, total_examples = doc2vec_model.corpus_count, epochs = doc2vec_model.epochs)
+
+    # save model
+    save_path = str(project_dir.joinpath(f'models/doc2vec_{num_epochs}_epochs.model'))
+    doc2vec_model.save(save_path)    
+
 
 def main():
     '''
-    Trains doc2vec model and saves to ../models
+    Trains doc2vec model, multiple times for various epochs, and saves to ../models
     '''
     logger = logging.getLogger(__name__)
-    logger.info('training doc2vec model from training data ../data/processed/train.pkl')
+    logger.info('training doc2vec models from training data ../data/processed/train.pkl')
 
     # load train.pkl
     train_path = project_dir.joinpath('data/processed/train.pkl')
@@ -26,21 +46,14 @@ def main():
     for index, record in enumerate(train['Tokenised']):
         train_tagged.append(gensim.models.doc2vec.TaggedDocument(record, [index]))
 
-    # doc2vec model (dm = 1 is Distributed Memory approach as used in Seidel et al., 2020, set seed for reproducibility)
-    doc2vec_model = gensim.models.doc2vec.Doc2Vec(vector_size = 256,
-                                                  dm = 1,
-                                                  min_count = 2,
-                                                  epochs = 40,
-                                                  seed = 1,
-                                                  workers = 1)
-
-    # build vocabulary and train model
-    doc2vec_model.build_vocab(train_tagged)
-    doc2vec_model.train(train_tagged, total_examples = doc2vec_model.corpus_count, epochs = doc2vec_model.epochs)
-
-    # save model
-    save_path = str(project_dir.joinpath(f'models/doc2vec.model'))
-    doc2vec_model.save(save_path)
+    train_doc2vec(1, train_tagged)
+    train_doc2vec(5, train_tagged)
+    train_doc2vec(10, train_tagged)
+    train_doc2vec(25, train_tagged)
+    train_doc2vec(50, train_tagged)
+    train_doc2vec(100, train_tagged)
+    train_doc2vec(200, train_tagged)
+    train_doc2vec(500, train_tagged)
 
 
 if __name__ == '__main__':
